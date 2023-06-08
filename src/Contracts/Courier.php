175e032f6e9322461e1c234fa6bd948db2691311
Robert <rtudor@palmcentre.co.uk>
2023-06-08 16:17:04 +0700

    added method to test the endpoint of a courier

--- src/Contracts/Courier.php
+++ src/Contracts/Courier.php
@@ -8,10 +8,9 @@ use Mcpuishor\CourierManager\Enums\LabelFileFormat;
 
 interface Courier
 {
-    public function get(): self;
-
     public function book(Booking $consignment): Consignment;
 
+    public function isEndpointAvailable() : bool;
     public function isServiceAvailable(Booking $booking) : bool;
 
     public function label(string $consignmentNumber, LabelFileFormat $format) : Label;
